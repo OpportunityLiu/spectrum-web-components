54af4438c3a49b21f4863c87c70b3d4e7d7f7354
Westbrook Johnson <westbrook.johnson@gmail.com>
2021-02-12 21:58:05 -0500

    chore: update lit-* dependencies, wip

--- packages/shared/src/observe-slot-text.ts
+++ packages/shared/src/observe-slot-text.ts
@@ -11,13 +11,14 @@ governing permissions and limitations under the License.
 */
 import {
     PropertyValues,
-    UpdatingElement,
+    ReactiveElement,
     queryAssignedNodes,
     property,
 } from '@spectrum-web-components/base';
 
 const slotElementObserver = Symbol('slotElementObserver');
-const assignedNodesList = Symbol('assignedNodes');
+// Fix needed for: https://github.com/lit/lit/issues/1789
+const assignedNodesList = 'longVersionOfAssignedNodesList'; //Symbol('assignedNodes');
 const startObserving = Symbol('startObserving');
 
 type Constructor<T = Record<string, unknown>> = {
@@ -31,11 +32,12 @@ export interface SlotTextObservingInterface {
     manageTextObservedSlot(): void;
 }
 
-export function ObserveSlotText<T extends Constructor<UpdatingElement>>(
+export function ObserveSlotText<T extends Constructor<ReactiveElement>>(
     constructor: T,
     slotSelector?: string
 ): T & Constructor<SlotTextObservingInterface> {
-    class SlotTextObservingElement extends constructor
+    class SlotTextObservingElement
+        extends constructor
         implements SlotTextObservingInterface {
         private [slotElementObserver]: MutationObserver;
 
